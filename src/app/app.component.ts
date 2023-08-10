1cd2268f2330ca1007a5ab44231609e15c9f89d1
Emile0001 <boteseh@gmail.com>
2023-08-10 14:44:03 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'SuperHero.UI';
+
+  heroes: SuperHero[] = [];
+
+  constructor(private superHeroServices: SuperHeroService) {}
+
+  ngOnInit(): void {
+    this.heroes = this.superHeroServices.getSuperHeroes();
+    console.log(this.heroes);
+  }
 }
